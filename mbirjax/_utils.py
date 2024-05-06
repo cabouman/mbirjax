2e5d0a007e8db6d661fafbf927e5b7b7d02d9c84
Charles Bouman <Charles.Bouman@gmail.com>
2024-05-06 08:19:42 -0400

    Remove prox_recon parameter

--- mbirjax/_utils.py
+++ mbirjax/_utils.py
@@ -14,11 +14,10 @@ _forward_model_defaults_dict = {
 
 _recon_model_defaults_dict = {
     'recon_shape': {'val': None, 'recompile_flag': True},
-    'prox_recon': {'val': None, 'recompile_flag': False},
+    'magnification': {'val': 1.0, 'recompile_flag': False},
     'delta_pixel_recon': {'val': 1.0, 'recompile_flag': False},
     'sigma_x': {'val': 1.0, 'recompile_flag': False},
     'sigma_p': {'val': 1.0, 'recompile_flag': False},
-    'magnification': {'val': 1.0, 'recompile_flag': False},
     'p': {'val': 2.0, 'recompile_flag': False},
     'q': {'val': 1.2, 'recompile_flag': False},
     'T': {'val': 1.0, 'recompile_flag': False},
