bc9095407dae2421719f1a066be8cc798925e943
Greg Buzzard <buzzard@purdue.edu>
2024-06-07 17:07:02 -0400

    Update qggmrf test.

--- tests/test_qggmrf.py
+++ tests/test_qggmrf.py
@@ -40,29 +40,29 @@ class TestQGGMRF(unittest.TestCase):
         c1 *= ds_q_minus_p * ((q / p) + ds_q_minus_p)
         b_tilde_ref = c1 / (1 + ds_q_minus_p) ** 2
 
-        assert(jnp.allclose(b_tilde, b_tilde_ref))
+        assert (jnp.allclose(b_tilde, b_tilde_ref))
 
     def test_gradient_and_hessian(self):
         gradient = (
-            np.array([[-0.24734181, -0.06174505, -0.2730214],
-                    [0.19395277, -0.12066687, 0.36728308],
-                    [0.25866383, 0.1520284, -0.2846858],
-                    [0.41440377, -0.37881848, 0.3841581],
-                    [-0.5139092, 0.5978173, -0.41555712],
-                    [-0.04249194, -0.48688126, 0.38089654],
-                    [-0.23896863, 0.22469066, -0.314788],
-                    [0.10269843, -0.13829419, 0.27461702],
-                    [0.28494033, 0.16065526, -0.27963576]], dtype=np.float32))
+            np.array([[-0.28758478, 0.17966035, 0.06372651],
+                      [0.21930355, 0.42724127, -0.33447355],
+                      [-0.03735046, -0.3333393, 0.04014717],
+                      [0.4248102, -0.4625442, -0.21327719],
+                      [-0.54877305, 0.2317356, 0.04053728],
+                      [0.19778192, 0.42639467, 0.14354284],
+                      [-0.2730683, 0.41404432, -0.17601134],
+                      [0.3569668, -0.5147747, 0.40523687],
+                      [-0.09706858, -0.00686641, -0.2859974]], dtype=np.float32))
         hessian = (
-            np.array([[1.53219031e+03, 1.02421265e+03, 1.53115833e+03],
-                    [1.02177649e+03, 5.13915710e+02, 1.02134473e+03],
-                    [1.53115247e+03, 1.02255969e+03, 1.53082593e+03],
-                    [1.02077545e+03, 5.12037476e+02, 1.02113623e+03],
-                    [5.11005737e+02, 1.41176522e+00, 5.12126953e+02],
-                    [1.02121881e+03, 5.11294250e+02, 1.02108868e+03],
-                    [1.53138721e+03, 1.02211853e+03, 1.53054639e+03],
-                    [1.02421106e+03, 5.18873352e+02, 1.02465173e+03],
-                    [1.53083044e+03, 1.02169562e+03, 1.53087988e+03]], dtype=np.float32))
+            np.array([[1530.8113, 1021.26227, 1531.6426],
+                      [1021.8176, 512.0654, 1021.831],
+                      [1531.7743, 1021.9556, 1531.9774],
+                      [1020.6803, 512.4941, 1023.63727],
+                      [510.69043, 2.4552107, 513.4677],
+                      [1021.80176, 511.99084, 1022.6514],
+                      [1530.9652, 1020.7792, 1531.6259],
+                      [1021.4198, 510.99796, 1020.9037],
+                      [1531.5032, 1021.65314, 1530.8649]], dtype=np.float32))
 
         np.random.seed(0)
         p = np.random.rand(1)[0] + 1
@@ -77,9 +77,10 @@ class TestQGGMRF(unittest.TestCase):
         flat_recon = flat_recon.reshape((recon_shape[0] * recon_shape[1], recon_shape[2]))
         pixel_indices = np.arange(flat_recon.shape[0])
 
-        grad, hess = mbirjax.qggmrf_gradient_and_hessian_at_indices(flat_recon, recon_shape, pixel_indices, qggmrf_params)
-        assert(jnp.allclose(grad, gradient))
-        assert(jnp.allclose(hess, hessian))
+        grad, hess = mbirjax.qggmrf_gradient_and_hessian_at_indices(flat_recon, recon_shape, pixel_indices,
+                                                                    qggmrf_params)
+        assert (jnp.allclose(grad, gradient))
+        assert (jnp.allclose(hess, hessian))
 
 
 if __name__ == '__main__':
