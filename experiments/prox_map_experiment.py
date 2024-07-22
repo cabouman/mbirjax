4c5543118616def1e5f304b8ade42b0ff892af4c
Greg Buzzard <buzzard@purdue.edu>
2024-07-22 14:06:53 -0400

    Update to match prerelease.

---
+++ experiments/prox_map_experiment.py
@@ -0,0 +1,74 @@
+import numpy as np
+import time
+import jax.numpy as jnp
+import mbirjax.parallel_beam
+
+if __name__ == "__main__":
+    """
+    This is a script to develop, debug, and tune the parallel beam mbirjax code.
+    """
+    # Choose the geometry type
+    geometry_type = 'cone'
+
+    print('Using {} geometry.'.format(geometry_type))
+
+    # Set parameters
+    num_views = 1
+    view_angle = np.pi / 2
+    num_det_rows = 40
+    num_det_channels = 64
+    sharpness = 4.0
+    
+    # These can be adjusted to describe the geometry in the cone beam case.
+    # np.Inf is an allowable value, in which case this is essentially parallel beam
+    source_detector_dist = 4 * num_det_channels
+    source_iso_dist = source_detector_dist
+
+    detector_cone_angle = 2 * np.arctan2(num_det_channels / 2, source_detector_dist)
+
+    start_angle = view_angle
+    end_angle = view_angle + (np.pi + detector_cone_angle)
+
+    # Initialize sinogram
+    sinogram_shape = (num_views, num_det_rows, num_det_channels)
+    angles = jnp.linspace(start_angle, end_angle, num_views, endpoint=False)
+
+    # Set up the model
+    ct_model = mbirjax.ConeBeamModel(sinogram_shape, angles, source_detector_dist=source_detector_dist, source_iso_dist=source_iso_dist)
+
+    # Generate 3D Shepp Logan phantom
+    print('Creating phantom')
+    phantom = ct_model.gen_modified_3d_sl_phantom()
+
+    # Generate synthetic sinogram data
+    print('Creating sinogram')
+    sinogram = ct_model.forward_project(phantom)
+
+    # View sinogram
+    mbirjax.slice_viewer(sinogram, title='Original sinogram', slice_axis=0, slice_label='View')
+
+    # Generate weights array - for an initial reconstruction, use weights = None, then modify as desired.
+    weights = None
+    # weights = ct_model.gen_weights(sinogram / sinogram.max(), weight_type='transmission_root')
+
+    # Set reconstruction parameter values
+    ct_model.set_params(sharpness=sharpness, verbose=1)
+
+    # Print out model parameters
+    ct_model.print_params()
+
+    # ##########################
+    # Perform VCD reconstruction
+    prox_input = phantom  # jnp.zeros_like(phantom)
+    time0 = time.time()
+    recon, recon_params = ct_model.prox_map(prox_input, sinogram, weights=weights, num_iterations=10, init_recon=prox_input)
+
+    recon.block_until_ready()
+    elapsed = time.time() - time0
+    print('Elapsed time for recon is {:.3f} seconds'.format(elapsed))
+    # ##########################
+
+    # Display results
+    mbirjax.slice_viewer(phantom-recon, title='Phantom (left) vs VCD Recon (right)', vmin=-0.1, vmax=0.5)
+
+
---
+++ tests/test_prox.py
@@ -0,0 +1,65 @@
+import numpy as np
+import jax
+import jax.numpy as jnp
+import mbirjax
+import unittest
+
+
+class TestProx(unittest.TestCase):
+    """
+    Test the proximal map function for the special case when the input and initial condition are both the ground truth.
+    """
+
+    def setUp(self):
+        """Set up before each test method."""
+        np.random.seed(0)  # Set a seed to avoid variations due to partition creation.
+
+        # Set parameters
+        self.num_views = 32
+        self.num_det_rows = 40
+        self.num_det_channels = 128
+        self.sharpness = 0.0
+
+        # Initialize sinogram
+        self.sinogram_shape = (self.num_views, self.num_det_rows, self.num_det_channels)
+        self.angles = None
+
+    def tearDown(self):
+        """Clean up after each test method."""
+        pass
+
+    def test_prox(self):
+        """
+        Verify that the proximal map function works for a simple corner case.
+        """
+        start_angle = 0.0
+        end_angle = np.pi
+
+        self.angles = jnp.linspace(start_angle, end_angle, self.num_views, endpoint=False)
+        ct_model = mbirjax.ParallelBeamModel(self.sinogram_shape, self.angles)
+
+        # Generate 3D Shepp Logan phantom
+        print('  Creating phantom')
+        phantom = ct_model.gen_modified_3d_sl_phantom()
+
+        # Generate synthetic sinogram data
+        print('  Creating sinogram')
+        sinogram = ct_model.forward_project(phantom)
+
+        # Set reconstruction parameter values
+        ct_model.set_params(verbose=0)
+
+        # ##########################
+        # Evaluating the proximal map
+        print('  Starting proximal map')
+        prox_input = phantom
+        recon, recon_params = ct_model.prox_map(prox_input, sinogram, num_iterations=5, init_recon=prox_input)
+        recon.block_until_ready()
+
+        max_diff = np.amax(np.abs(phantom - recon))
+        tolerance = 1e-5
+        self.assertTrue(max_diff < tolerance)
+
+
+if __name__ == '__main__':
+    unittest.main()
