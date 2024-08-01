b9147bb65b99551797d372c290e019528f37ff1f
Greg Buzzard <buzzard@purdue.edu>
2024-08-01 09:03:39 -0400

    Improve display and include todo list.

--- experiments/gpu_performance/display_results.py
+++ experiments/gpu_performance/display_results.py
@@ -30,25 +30,59 @@ def display_results(filename):
     vmax_time = 5
 
     mem0 = mem_values.flatten()[0]
-    time0 = time_values.flatten()[0]
     mem_values /= mem0
+
+    time_values = normalize_time_values(time_values, outer_labels, outer_values, inner_labels, inner_values)
+    time0 = time_values.flatten()[0]
     time_values /= time0
 
     plt.ion()
 
     eval_types = ['Forward_projection', 'Backward_projection']
     super_title_mem = eval_types[eval_type_index]
-    super_title_mem += '\nlog2(GB / GB_baseline) as a function of #views, #channels, #rows, and #indices'
+    super_title_mem += '\nlog2(GB / GB_baseline) for one batch as a function of #rows, #channels, #views, and #indices'
     super_title_mem += '\nGB_baseline = {:.2f}GB'.format(mem0)
     # super_title_mem += ':  Avail GB = {:.1f}, Max percent mem used = {:.2f}'.format(max_avail_gb, max_percent_used_gb)
     super_title_time = eval_types[eval_type_index]
-    super_title_time += '\nlog2(seconds elapsed / secs_baseline) as a function of #views, #channels, #rows, and #indices'
+    super_title_time += '\nlog2(seconds elapsed / secs_baseline) for full set of batches as a function of #rows, #channels, #views, and #indices'
     super_title_time += '\nsecs_baseline elapsed = {:.3g} secs'.format(time0)
 
     create_tiled_heatmap(np.log2(mem_values), outer_labels, outer_values, inner_labels, inner_values,
                          super_title=super_title_mem, vmin=vmin_mem, vmax=vmax_mem)
     create_tiled_heatmap(np.log2(time_values), outer_labels, outer_values, inner_labels, inner_values,
                          super_title=super_title_time, vmin=vmin_time, vmax=vmax_time)
+
+
+def normalize_time_values(time_values, outer_labels, outer_values, inner_labels, inner_values):
+    """
+    Each entry in time_values gives the time needed for a batch of views and indices for a given umber of detector rows
+    and channels.  Assuming a recon size of num_channels**2 * num_rows and a sinogram with num_channels views, this
+    function multiplies each entry by the number of reps needed to project the full recon and sino.
+    Args:
+        time_values:
+        outer_labels:
+        outer_values:
+        inner_labels:
+        inner_values:
+
+    Returns:
+
+    """
+    row_index = 0
+    channel_index = 1
+    assert(outer_labels[row_index] == '# det rows' and outer_labels[channel_index] == '# det channels')
+    view_index = 0
+    indices_index = 1
+    assert(inner_labels[view_index] == '# views' and inner_labels[indices_index] == '# indices')
+    for i, num_row in enumerate(outer_values[row_index]):
+        for j, num_channels in enumerate(outer_values[channel_index]):
+            total_indices = num_channels * num_channels * 0.8  # Approx # pixels in the region of reconstruction
+            total_views = num_channels
+            for k, num_views in enumerate(inner_values[view_index]):
+                for n, num_indices in enumerate(inner_values[indices_index]):
+                    num_reps = (total_views / num_views) * (total_indices / num_indices)
+                    time_values[i, j, k, n] *= num_reps
+    return time_values
 
 
 def create_tiled_heatmap(plot_values, outer_labels, outer_values, inner_labels, inner_values,
@@ -66,10 +100,10 @@ def create_tiled_heatmap(plot_values, outer_labels, outer_values, inner_labels, 
 
     Args:
         plot_values (4D numpy array):  Values to display
-        outer_labels (length 2 string): The labels describing axes 0 and 1 of plot_values (e.g., #views, #channels)
-        outer_values (length 2 list):  outer_values[0] (or [1]) contains the values of the independent variable for axis 0 (or 1)
-        inner_labels (length 2 string): The labels describing axes 2 and 3 of plot_values (e.g., #slices, #indices)
-        inner_values (length 2 list): inner_values[0] (or [1]) contains the values of the independent variable for axis 2 (or 3)
+        outer_labels (length 2 list of strings): The labels describing axes 0 and 1 of plot_values (e.g., #views, #channels)
+        outer_values (length 2 list of lists of int):  outer_values[0] (or [1]) contains the values of the independent variable for axis 0 (or 1)
+        inner_labels (length 2 list of strings): The labels describing axes 2 and 3 of plot_values (e.g., #slices, #indices)
+        inner_values (length 2 list of lists int): inner_values[0] (or [1]) contains the values of the independent variable for axis 2 (or 3)
         sup_title (string, optional): Overall title for the display
         vmin (float, optional): lower clipping value for colorbar display
         vmax (float, optional): upper clipping value for colorbar display
