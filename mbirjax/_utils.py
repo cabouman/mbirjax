bfb06e4da574eb994ae983a45256ad0b09e25ec6
Greg Buzzard <buzzard@purdue.edu>
2024-08-04 13:47:34 -0400

    Update granularity.

--- mbirjax/_utils.py
+++ mbirjax/_utils.py
@@ -36,8 +36,8 @@ _reconstruction_defaults_dict = {
     'positivity_flag': {'val': False, 'recompile_flag': False},
     'snr_db': {'val': 30.0, 'recompile_flag': False},
     'sharpness': {'val': 0.0, 'recompile_flag': False},
-    'granularity': {'val': [1, 4, 64, 96], 'recompile_flag': False},
-    'partition_sequence': {'val': [0, 1, 2, 3], 'recompile_flag': False},
+    'granularity': {'val': [1, 8, 48, 96], 'recompile_flag': False},
+    'partition_sequence': {'val': [0, 0, 1, 1, 2, 3], 'recompile_flag': False},
     'verbose': {'val': 1, 'recompile_flag': False}
 }
 
