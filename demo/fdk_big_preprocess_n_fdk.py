21e015a86ebd80d3f0eb1a85dface63a30936ce5
ZiyunLiiii <ziyunliiii@gmail.com>
2025-02-12 14:59:39 -0500

    two demo file: one loads the preprocessed sinogram and recon; the other preprocesses and does fdk recon

--- demo/fdk_big_preprocess_n_fdk.py
+++ demo/fdk_big_preprocess_n_fdk.py
@@ -2,10 +2,7 @@ import numpy as np
 import os
 import time
 import pprint
-import jax.numpy as jnp
 import pprint
-import jax, scipy, pickle
-import sys
 import mbirjax
 
 os.environ['XLA_PYTHON_CLIENT_MEM_FRACTION'] = '0.98'
@@ -33,15 +30,17 @@ subsample_view_factor = 1  # view subsample factor.
 sharpness = 0.0
 # ###################### End of parameters
 
-sino = np.load('/home/li5273/PycharmProjects/mbirjax_applications/nsi/demo_data/sino.npy')
+print("\n*******************************************************",
+        "\n************** NSI dataset preprocessing **************",
+        "\n*******************************************************")
+sino, cone_beam_params, optional_params = \
+    mbirjax.preprocess.nsi.compute_sino_and_params(dataset_dir,
+                                                    downsample_factor=downsample_factor,
+                                                    subsample_view_factor=subsample_view_factor)
 
-with open("/home/li5273/PycharmProjects/mbirjax_applications/nsi/demo_data/optionalparams.pkl", "rb") as f:
-    optional_params = pickle.load(f)
-
-with open("/home/li5273/PycharmProjects/mbirjax_applications/nsi/demo_data/conebeamparams.pkl", "rb") as f:
-    cone_beam_params = pickle.load(f)
-
-
+print("\n*******************************************************",
+        "\n***************** Set up MBIRJAX model ****************",
+        "\n*******************************************************")
 # ConeBeamModel constructor
 ct_model = mbirjax.ConeBeamModel(**cone_beam_params)
 
@@ -69,10 +68,6 @@ time0 = time.time()
 init_recon = ct_model.fdk_recon(sino)
 
 
-mbirjax.preprocess.export_recon_to_hdf5(init_recon, os.path.join(output_path, "recon_fdk11.h5"),
-                                        recon_description="MBIRJAX recon of MAR phantom",
-                                        alu_description="1 ALU = 0.508 mm")
-
 # change the image data shape to (slices, rows, cols), so that the rotation axis points up when viewing the
 # coronal/sagittal slices with slice_viewer
 init_recon = np.transpose(init_recon, (2, 1, 0))
@@ -84,35 +79,4 @@ vmax = downsample_factor[0] * 0.008
 mbirjax.slice_viewer(init_recon, vmin=0, vmax=vmax, slice_axis=0, slice_label='Axial Slice', title='MBIRJAX recon')
 mbirjax.slice_viewer(init_recon, vmin=0, vmax=vmax, slice_axis=1, slice_label='Coronal Slice', title='MBIRJAX recon')
 mbirjax.slice_viewer(init_recon, vmin=0, vmax=vmax, slice_axis=2, slice_label='Sagittal Slice', title='MBIRJAX recon')
-
-
-recon, recon_params = ct_model.recon(sino, weights=weights, init_recon=init_recon)
-
-recon.block_until_ready()
-elapsed = time.time() - time0
-print('Elapsed time for recon is {:.3f} seconds'.format(elapsed))
-# ##########################
-
-# Print out parameters used in recon
-pprint.pprint(recon_params._asdict())
-
-mbirjax.preprocess.export_recon_to_hdf5(recon, os.path.join(output_path, "recon_vcd_11.h5"),
-                                        recon_description="MBIRJAX recon of MAR phantom",
-                                        alu_description="1 ALU = 0.508 mm")
-
-# change the image data shape to (slices, rows, cols), so that the rotation axis points up when viewing the
-# coronal/sagittal slices with slice_viewer
-recon = np.transpose(recon, (2, 1, 0))
-recon = recon[:, :, ::-1]
-
-vmin = 0
-vmax = downsample_factor[0] * 0.008
-# Display results
-mbirjax.slice_viewer(recon, vmin=0, vmax=vmax, slice_axis=0, slice_label='Axial Slice', title='MBIRJAX recon')
-mbirjax.slice_viewer(recon, vmin=0, vmax=vmax, slice_axis=1, slice_label='Coronal Slice', title='MBIRJAX recon')
-mbirjax.slice_viewer(recon, vmin=0, vmax=vmax, slice_axis=2, slice_label='Sagittal Slice', title='MBIRJAX recon')
-
-
-
-
 
