88a8d1e0272542c1eafee198808021ea6b490faa
gbuzzard <buzzard@purdue.edu>
2025-03-14 15:51:08 -0400

    Improve variable names.

--- tests/test_compute_sino_from_scans.py
+++ tests/test_compute_sino_from_scans.py
@@ -37,7 +37,7 @@ class TestNSIPreprocessing(unittest.TestCase):
 
         Parameters:
         - shape (tuple): Shape of the dark scan (e.g., (height, width)).
-        - mean (float): Mean of the Gaussian noise (default: 0.0).
+        - mean (float or jax Array): Mean of the Gaussian noise (default: 0.0).
         - stddev (float): Standard deviation of the Gaussian noise (default: 0.01).
         - clip_negative (bool): Whether to clip negative values to zero (default: True).
 
@@ -49,17 +49,18 @@ class TestNSIPreprocessing(unittest.TestCase):
         # Generate Gaussian noise
         dark_scan = np.random.normal(mean, stddev, size=shape)
 
-        # Clip negative values if needed
+        # We should get no negative values.  If we do, then we take the absolute value instead of
+        # clipping so that the values are still random.
         if clip_negative:
-            dark_scan = np.clip(dark_scan, 0, None)
+            dark_scan = np.abs(dark_scan, 0, None)
 
         return dark_scan
 
     def setUp(self):
         """Set up parameters and initialize models before each test."""
         # Sinogram parameters
-        self.num_views = 128
-        self.num_det_rows = 128
+        self.num_views = 40
+        self.num_det_rows = 64
         self.num_det_channels = 128
         self.sinogram_shape = (self.num_views, self.num_det_rows, self.num_det_channels)
 
@@ -68,27 +69,30 @@ class TestNSIPreprocessing(unittest.TestCase):
         self.source_iso_dist = self.source_detector_dist / 2
         start_angle = -jnp.pi  # For testing purposes, we use a full 360 degrees
         end_angle = jnp.pi
-        self.fdk_angles = jnp.linspace(start_angle, end_angle, self.num_views, endpoint=False)
+        self.angles = jnp.linspace(start_angle, end_angle, self.num_views, endpoint=False)
         self.maximum_intensity = 4.0
 
         # Initialize CT model
         self.cone_model = mbirjax.ConeBeamModel(self.sinogram_shape,
-                                                self.fdk_angles,
+                                                self.angles,
                                                 source_detector_dist=self.source_detector_dist,
                                                 source_iso_dist=self.source_iso_dist)
 
         # Generate 3D Shepp-Logan phantom and sinogram
         self.phantom = self.cone_model.gen_modified_3d_sl_phantom()
-        self.sino_gdt = self.cone_model.forward_project(self.phantom)
+        self.sino_gt = self.cone_model.forward_project(self.phantom)
         # Normalize the sinogram
-        self.sino_gdt = self.sino_gdt / np.percentile(self.sino_gdt, 98)
-        self.ideal_obj_scan = self.maximum_intensity * np.exp(-self.sino_gdt)
+        self.sino_gt = self.sino_gt / jnp.percentile(self.sino_gt, 98)
+        self.ideal_obj_scan = self.maximum_intensity * jnp.exp(-self.sino_gt)
         # Set the mean and standard deviation for the dark scan
-        mean = np.mean(self.ideal_obj_scan) / 100
-        stddev = 0.001
+        # These values are estimated empirically from NSI data.
+        dark_mean = 0.02
+        dark_stddev = 0.001
 
-        self.blank_scan = self.maximum_intensity + self.generate_dark_scan(self.sinogram_shape, mean=mean, stddev=stddev, seed=42)
-        self.obj_scan = self.ideal_obj_scan + self.generate_dark_scan(self.sinogram_shape[1:], mean=mean, stddev=stddev, seed=43)
+        # Create blank scan using the maximum intensity plus a realization of the dark scan.
+        # Then repeat for the object scan, using the noise-free scan plus a new dark scan.
+        self.blank_scan = self.maximum_intensity + self.generate_dark_scan(self.sinogram_shape, mean=dark_mean, stddev=dark_stddev, seed=42)
+        self.obj_scan = self.ideal_obj_scan + self.generate_dark_scan(self.sinogram_shape[1:], mean=dark_mean, stddev=dark_stddev, seed=43)
 
         # Randomly generate two defective pixel coordinates with length 2 and 3 to test the function's ability to handle different coordinate lengths
         np.random.seed(25)
@@ -96,7 +100,7 @@ class TestNSIPreprocessing(unittest.TestCase):
         # Randomly set pixel to negative value to test the function's ability to handle negative object scan values
         self.obj_scan = self.obj_scan.at[np.random.randint(0, self.num_views-1), np.random.randint(0, self.num_det_rows-1), np.random.randint(0, self.num_det_channels-1)].set(-1)
 
-        self.dark_scan = self.generate_dark_scan(self.sinogram_shape, mean=mean, stddev=stddev, seed=44)
+        self.dark_scan = self.generate_dark_scan(self.sinogram_shape, mean=dark_mean, stddev=dark_stddev, seed=44)
 
         self.compute_sino_tolerance = {'atol': 1.17, 'mean_tol': 0.0012}
         self.estimate_bg_tolerance = {'atol': 1.17, 'mean_tol': 0.0012}
@@ -106,8 +110,8 @@ class TestNSIPreprocessing(unittest.TestCase):
         sino_computed, _ = compute_sino_transmission_jax(self.obj_scan, self.blank_scan, self.dark_scan, defective_pixel_list=self.defective_pixel_list)
         # Compare sinograms
         # Compute differences
-        max_diff = np.max(np.abs(sino_computed - self.sino_gdt))
-        mean_diff = np.mean(np.abs(sino_computed - self.sino_gdt))
+        max_diff = np.max(np.abs(sino_computed - self.sino_gt))
+        mean_diff = np.mean(np.abs(sino_computed - self.sino_gt))
         tolerance = self.compute_sino_tolerance['atol']
         tolerance_mean = self.compute_sino_tolerance['mean_tol']
 
@@ -129,8 +133,8 @@ class TestNSIPreprocessing(unittest.TestCase):
         print("background_offset = ", background_offset)
         sino_computed = sino_computed - background_offset
 
-        max_diff = np.max(np.abs(sino_computed - self.sino_gdt))
-        mean_diff = np.mean(np.abs(sino_computed - self.sino_gdt))
+        max_diff = np.max(np.abs(sino_computed - self.sino_gt))
+        mean_diff = np.mean(np.abs(sino_computed - self.sino_gt))
         tolerance = self.estimate_bg_tolerance['atol']
         tolerance_mean = self.estimate_bg_tolerance['mean_tol']
 
