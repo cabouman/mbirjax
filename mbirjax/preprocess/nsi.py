37796a2baf908c41624dd0df471114305ba35cce
Greg Buzzard <buzzard@purdue.edu>
2025-03-17 13:39:01 -0400

    Refactor to improve modularization in getting and setting parameters.

--- mbirjax/preprocess/nsi.py
+++ mbirjax/preprocess/nsi.py
@@ -1,5 +1,6 @@
 import os, sys
 import re
+from operator import itemgetter
 import numpy as np
 import warnings
 import mbirjax.preprocess as preprocess
@@ -69,37 +70,49 @@ def compute_sino_and_params(dataset_dir,
 
     """
 
-    print("\n\n########## Loading object, blank, dark scans, and geometry parameters from NSI dataset directory ...")
-    obj_scan, blank_scan, dark_scan, cone_beam_params, optional_params, defective_pixel_array = \
-            load_scans_and_params(dataset_dir,
-                                  downsample_factor=downsample_factor, crop_region=crop_region,
-                                  subsample_view_factor=subsample_view_factor)
+    print("\n\n########## Loading object, blank, dark scans, and geometry parameters from NSI dataset directory")
+    obj_scan, blank_scan, dark_scan, nsi_params, defective_pixel_array = \
+            load_scans_and_params(dataset_dir, subsample_view_factor=subsample_view_factor)
 
-    print("MBIRJAX geometry parameters:")
-    pp.pprint(cone_beam_params)
-    pp.pprint(optional_params)
-    print('obj_scan shape = ', obj_scan.shape)
-    print('blank_scan shape = ', blank_scan.shape)
-    print('dark_scan shape = ', dark_scan.shape)
+    cone_beam_params, optional_params = convert_nsi_to_mbirjax_params(nsi_params, downsample_factor=downsample_factor,
+                                                                      crop_region=crop_region)
 
-    print("\n\n########## Computing sinogram from object, blank, and dark scans ...")
+    pprint("\n\n########## Cropping and downsampling scans")
+    ### crop the scans based on input params
+    obj_scan, blank_scan, dark_scan, defective_pixel_array = preprocess.crop_scans(obj_scan, blank_scan, dark_scan,
+                                                                                   crop_region=crop_region,
+                                                                                   defective_pixel_array=defective_pixel_array)
+
+    ### downsample the scans with block-averaging
+    if downsample_factor[0]*downsample_factor[1] > 1:
+        obj_scan, blank_scan, dark_scan, defective_pixel_array = preprocess.downsample_scans(obj_scan, blank_scan, dark_scan,
+                                                                                             downsample_factor=downsample_factor,
+                                                                                             defective_pixel_array=defective_pixel_array)
+
+    print("\n\n########## Computing sinogram from object, blank, and dark scans")
     sino = preprocess.compute_sino_transmission_jax(obj_scan, blank_scan, dark_scan, defective_pixel_array)
-    del obj_scan, blank_scan, dark_scan # delete scan images to save memory
+    scan_shapes = obj_scan.shape, blank_scan.shape, dark_scan.shape
+    del obj_scan, blank_scan, dark_scan  # delete scan images to save memory
 
-    print("\n\n########## Estimating background offset to the sinogram from edge pixels ...")
+    print("\n\n########## Correcting sinogram data to account for background offset and detector rotation")
     background_offset = preprocess.estimate_background_offset_jax(sino)
     print("background_offset = ", background_offset)
 
-    print("\n\n########## Correcting sinogram data to account for background offset and detector rotation ...")
     det_rotation = optional_params["det_rotation"]
     sino = preprocess.correct_det_rotation_and_background(sino, det_rotation=det_rotation, background_offset=background_offset)
     del optional_params["det_rotation"]  # We delete this since it's not an allowed parameter in TomographyModel.
 
+    # print("MBIRJAX geometry parameters:")
+    # pp.pprint(cone_beam_params)
+    # pp.pprint(optional_params)
+    print('obj_scan shape = ', scan_shapes[0])
+    print('blank_scan shape = ', scan_shapes[1])
+    print('dark_scan shape = ', scan_shapes[2])
+
     return sino, cone_beam_params, optional_params
 
 
-def load_scans_and_params(dataset_dir, downsample_factor=(1, 1), crop_region=((0, 1), (0, 1)),
-                          view_id_start=0, view_id_end=None, subsample_view_factor=1):
+def load_scans_and_params(dataset_dir, view_id_start=0, view_id_end=None, subsample_view_factor=1):
     """
     Load the object scan, blank scan, dark scan, view angles, defective pixel information, and geometry parameters from an NSI scan directory.
 
@@ -115,10 +128,6 @@ def load_scans_and_params(dataset_dir, downsample_factor=(1, 1), crop_region=((0
             - ``**/offset.tif`` (dark scan image)
             - ``**/*.defect`` (defective pixel information)
 
-        downsample_factor ((int, int), optional) - Down-sample factors along the detector rows and channels respectively.
-            If scan size is not divisible by `downsample_factor`, the scans will be first truncated to a size that is divisible by `downsample_factor`.
-        crop_region (((float, float),(float, float)), optional) - Values of ((row_start, row_end), (col_start, col_end)) define a bounding box that crops the scan.
-            The default of ((0, 1), (0, 1)) retains the entire scan.
         view_id_start (int, optional): view index corresponding to the first view.
         view_id_end (int, optional): view index corresponding to the last view. If None, this will be equal to the total number of object scan images in ``obj_scan_dir``.
         subsample_view_factor (int, optional): view subsample factor.
@@ -129,8 +138,7 @@ def load_scans_and_params(dataset_dir, downsample_factor=(1, 1), crop_region=((0
             obj_scan (jax array): 3D object scan with shape (num_views, num_det_rows, num_det_channels).
             blank_scan (jax array): 3D blank scan with shape (1, num_det_rows, num_det_channels).
             dark_scan (jax array): 3D dark scan with shape (1, num_det_rows, num_det_channels).
-            cone_beam_params (dict): Required parameters for the ConeBeamModel constructor.
-            optional_params (dict): Additional ConeBeamModel parameters to be set using set_params().
+            nsi_params (dict): Required parameters needed for convert_nsi_to_mbirjax_params().
             defective_pixel_array (ndarray): An nx2 array containing indices of invalid sinogram pixels, with the format (detector_row_idx, detector_channel_idx).
     """
     ### automatically parse the paths to NSI metadata and scans from dataset_dir
@@ -251,36 +259,6 @@ def load_scans_and_params(dataset_dir, downsample_factor=(1, 1), crop_region=((0
     print("############ End NSI geometry parameters ############")
     ### END load NSI parameters from an nsipro file
 
-
-    ### Convert NSI geometry parameters to MBIR parameters
-    source_detector_dist, source_iso_dist, magnification, det_rotation = calc_source_detector_params(r_a, r_n, r_h, r_s, r_r)
-
-    det_channel_offset, det_row_offset = calc_row_channel_params(r_a, r_n, r_h, r_s, r_r, delta_det_channel, delta_det_row, num_det_channels, num_det_rows, magnification)
-
-    ### END Convert NSI geometry parameters to MBIR parameters
-
-    ### Adjust geometry NSI_params according to crop_region and downsample_factor
-    if isinstance(crop_region[0], (list, tuple)):
-        (row0, row1), (col0, col1) = crop_region
-    else:
-        row0, row1, col0, col1 = crop_region
-
-    ### Adjust detector size and pixel pitch params w.r.t. downsampling arguments
-    num_det_rows = num_det_rows // downsample_factor[0]
-    num_det_channels = num_det_channels // downsample_factor[1]
-
-    delta_det_row *= downsample_factor[0]
-    delta_det_channel *= downsample_factor[1]
-
-    ### Adjust detector size params w.r.t. cropping arguments
-    num_det_rows_shift0 = np.round(num_det_rows * row0)
-    num_det_rows_shift1 = np.round(num_det_rows * (1 - row1))
-    num_det_rows = num_det_rows - (num_det_rows_shift0 + num_det_rows_shift1)
-
-    num_det_channels_shift0 = np.round(num_det_channels * col0)
-    num_det_channels_shift1 = np.round(num_det_channels * (1 - col1))
-    num_det_channels = num_det_channels - (num_det_channels_shift0 + num_det_channels_shift1)
-
     ### read blank scans and dark scans
     blank_scan = np.expand_dims(preprocess.read_scan_img(blank_scan_path), axis=0)
     if dark_scan_path is not None:
@@ -307,7 +285,7 @@ def load_scans_and_params(dataset_dir, downsample_factor=(1, 1), crop_region=((0
 
     ### flip the scans according to flipH and flipV information from nsipro file
     if flipV:
-        print("Flip scans vertically!")
+        print("Flipping scans vertically")
         obj_scan = np.flip(obj_scan, axis=1)
         blank_scan = np.flip(blank_scan, axis=1)
         dark_scan = np.flip(dark_scan, axis=1)
@@ -316,7 +294,7 @@ def load_scans_and_params(dataset_dir, downsample_factor=(1, 1), crop_region=((0
             defective_pixel_array[:, 0] = blank_scan.shape[1] - defective_pixel_array[:, 0] - 1
 
     if flipH:
-        print("Flip scans horizontally!")
+        print("Flipping scans horizontally")
         obj_scan = np.flip(obj_scan, axis=2)
         blank_scan = np.flip(blank_scan, axis=2)
         dark_scan = np.flip(dark_scan, axis=2)
@@ -335,21 +313,71 @@ def load_scans_and_params(dataset_dir, downsample_factor=(1, 1), crop_region=((0
             defective_pixel_array = np.fliplr(defective_pixel_array)
             defective_pixel_array[:, 1] = blank_scan.shape[2] - defective_pixel_array[:, 1] - 1
 
-    ### crop the scans based on input params
-    obj_scan, blank_scan, dark_scan, defective_pixel_array = preprocess.crop_scans(obj_scan, blank_scan, dark_scan,
-                                                                                   crop_region=crop_region,
-                                                                                   defective_pixel_array=defective_pixel_array)
-
-    ### downsample the scans with block-averaging
-    if downsample_factor[0]*downsample_factor[1] > 1:
-        obj_scan, blank_scan, dark_scan, defective_pixel_array = preprocess.downsample_scans(obj_scan, blank_scan, dark_scan,
-                                                                                             downsample_factor=downsample_factor,
-                                                                                             defective_pixel_array=defective_pixel_array)
-
     ### compute projection angles based on angle_step and view_ids
     angles = np.deg2rad(np.array([(view_idx*angle_step) % 360.0 for view_idx in view_ids]))
 
-    ### Set 1 ALU = delta_det_channel
+    nsi_params = {
+        'r_a': r_a,
+        'r_n': r_n,
+        'r_h': r_h,
+        'r_s': r_s,
+        'r_r': r_r,
+        'delta_det_channel': delta_det_channel,
+        'delta_det_row': delta_det_row,
+        'num_det_channels': num_det_channels,
+        'num_det_rows': num_det_rows,
+        'angles': angles
+    }
+
+    return obj_scan, blank_scan, dark_scan, nsi_params, defective_pixel_array
+
+
+def convert_nsi_to_mbirjax_params(nsi_params, downsample_factor=(1, 1), crop_region=((0, 1), (0, 1)),):
+    """
+    Convert geometry parameters from nsi into mbirjax format, including modification to reflect crop and downsample.
+
+    Args:
+        nsi_params (dict):
+        downsample_factor ((int, int), optional) - Down-sample factors along the detector rows and channels respectively.
+            If scan size is not divisible by `downsample_factor`, the scans will be first truncated to a size that is divisible by `downsample_factor`.
+        crop_region (((float, float),(float, float)), optional) - Values of ((row_start, row_end), (col_start, col_end)) define a bounding box that crops the scan.
+            The default of ((0, 1), (0, 1)) retains the entire scan.
+
+    Returns:
+        cone_beam_params (dict): Required parameters for the ConeBeamModel constructor.
+        optional_params (dict): Additional ConeBeamModel parameters to be set using set_params().
+    """
+    # Get the nsi parameters and convert them
+    r_a, r_n, r_h, r_s, r_r = itemgetter('r_a', 'r_n', 'r_h', 'r_s', 'r_r')(nsi_params)
+    delta_det_channel, delta_det_row = itemgetter('delta_det_channel', 'delta_det_row')(nsi_params)
+    num_det_channels, num_det_rows, angles = itemgetter('num_det_channels', 'num_det_rows', 'angles')(nsi_params)
+
+    source_detector_dist, source_iso_dist, magnification, det_rotation = calc_source_detector_params(r_a, r_n, r_h, r_s, r_r)
+    det_channel_offset, det_row_offset = calc_row_channel_params(r_a, r_n, r_h, r_s, r_r, delta_det_channel, delta_det_row, num_det_channels, num_det_rows, magnification)
+
+    # Adjust geometry params according to crop_region and downsample_factor
+    if isinstance(crop_region[0], (list, tuple)):
+        (row0, row1), (col0, col1) = crop_region
+    else:
+        row0, row1, col0, col1 = crop_region
+
+    # Adjust detector size and pixel pitch params w.r.t. downsampling arguments
+    num_det_rows = num_det_rows // downsample_factor[0]
+    num_det_channels = num_det_channels // downsample_factor[1]
+
+    delta_det_row *= downsample_factor[0]
+    delta_det_channel *= downsample_factor[1]
+
+    # Adjust detector size params w.r.t. cropping arguments
+    num_det_rows_shift0 = np.round(num_det_rows * row0)
+    num_det_rows_shift1 = np.round(num_det_rows * (1 - row1))
+    num_det_rows = num_det_rows - (num_det_rows_shift0 + num_det_rows_shift1)
+
+    num_det_channels_shift0 = np.round(num_det_channels * col0)
+    num_det_channels_shift1 = np.round(num_det_channels * (1 - col1))
+    num_det_channels = num_det_channels - (num_det_channels_shift0 + num_det_channels_shift1)
+
+    # Set 1 ALU = delta_det_channel
     source_detector_dist /= delta_det_channel # mm to ALU
     source_iso_dist /= delta_det_channel # mm to ALU
     det_channel_offset /= delta_det_channel # mm to ALU
@@ -373,7 +401,7 @@ def load_scans_and_params(dataset_dir, downsample_factor=(1, 1), crop_region=((0
     optional_params["det_row_offset"] = det_row_offset
     optional_params["det_rotation"] = det_rotation # tilt angle of rotation axis
 
-    return obj_scan, blank_scan, dark_scan, cone_beam_params, optional_params, defective_pixel_array
+    return cone_beam_params, optional_params
 
 
 ######## subroutines for parsing NSI metadata
