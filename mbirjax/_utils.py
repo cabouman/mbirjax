588fe8d13833da1392312d190d29da389e07f71b
Charles Bouman <Charles.Bouman@gmail.com>
2025-04-10 15:08:22 -0400

    Simplify partition sequence

--- mbirjax/_utils.py
+++ mbirjax/_utils.py
@@ -37,7 +37,7 @@ _reconstruction_defaults_dict = {
     'snr_db': {'val': 30.0, 'recompile_flag': False},
     'sharpness': {'val': 1.0, 'recompile_flag': False},
     'granularity': {'val': [1, 2, 4, 8, 16, 32, 64, 128, 256], 'recompile_flag': False},
-    'partition_sequence': {'val': [0, 4, 6, 7, 7, 7, 7, 7, 7], 'recompile_flag': False},
+    'partition_sequence': {'val': [0, 4, 6, 7], 'recompile_flag': False},
     'verbose': {'val': 1, 'recompile_flag': False},
     'use_gpu': {'val': 'automatic', 'recompile_flag': True}  # Possible values are 'automatic', 'full', 'sinograms', 'projections', 'none'
 }
