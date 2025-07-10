f249db490154ff4aa4d2c09438f8ea7dc37ea44d
Caden Cardell <cadencardell@gmail.com>
2025-07-10 07:57:13 -0400

    snapshot view back projections

--- experiments/sharding/view_back_projections.py
+++ experiments/sharding/view_back_projections.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 
 ############################### CONTROL ###############################
 
-file_path = "output/control_back_projection_02d2da50.npy"
-expected_hash = "02d2da5081fec0ef3705b4b53e8a484e53a30d8040e60396b0616778b3cf8125"
+file_path = "output/control_back_projection_recon_27c4a43a.npy"
+expected_hash = "27c4a43acebc9e0e58a94ce67857b8b17a4aeb72b0146d494f6168e386dfb204"
 control_back_projection = np.load(file_path)
 
 hash_digest = hashlib.sha256(control_back_projection.tobytes()).hexdigest()
@@ -14,13 +14,13 @@ if hash_digest != expected_hash:
     print("\033[93m" + f"hash_digest of {file_path} is not the expected value." + "\033[0m")
     print("hash_digest:", hash_digest)
 
-mj.slice_viewer(control_back_projection, slice_axis=0, title='Control Back Projection')
+mj.slice_viewer(control_back_projection, slice_axis=2, title='Control Back Projection')
 
 
 ############################### SHARDED ###############################
 
-file_path = "output/sharded_back_projection_caa1f1ee.npy"
-expected_hash = "caa1f1ee9324007a2256ee887289dc9ad2903bfb5aa2f2d49ecfe2fd0f97cf1e"
+file_path = "output/sharding_back_projection_recon_9470fe24.npy"
+expected_hash = "9470fe2464cc4ce386fabb635c4914faf1e0b0a56d5ad465974bf35d4c4a2004"
 sharded_back_projection = np.load(file_path)
 
 hash_digest = hashlib.sha256(sharded_back_projection.tobytes()).hexdigest()
@@ -28,13 +28,13 @@ if hash_digest != expected_hash:
     print("\033[93m" + f"hash_digest of {file_path} is not the expected value." + "\033[0m")
     print("hash_digest:", hash_digest)
 
-mj.slice_viewer(sharded_back_projection, slice_axis=0, title='Sharded Back Projection')
+mj.slice_viewer(sharded_back_projection, slice_axis=2, title='Sharded Back Projection')
 
 
 ############################### DIFFERENCE ###############################
 
 difference_back_projection = control_back_projection - sharded_back_projection
-mj.slice_viewer(difference_back_projection, slice_axis=0, title='Difference Back Projection')
+mj.slice_viewer(difference_back_projection, slice_axis=2, title='Difference Back Projection')
 
 # stats
 print(f"Maximum Difference = {difference_back_projection.max()}")
