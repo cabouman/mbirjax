Fix bug in delta_recon_row calculation

mbirjax/translation_model.py
@@ -166,7 +166,7 @@ class TranslationModel(mbirjax.TomographyModel):
         # This results in isotropic voxels when cone_angle/2 > 63 deg
         nominal_row_pitch = det_pixel_pitch_iso_vec/(cone_slope/2.0)
         nominal_row_pitch = jnp.max(nominal_row_pitch)  # Take the maximum of the nominal pitches along x and z
-        delta_recon_row = jnp.minimum(nominal_row_pitch, det_pixel_pitch_iso) # Ensure that the row resolution is not higher than the (x,z) detector resolution
+        delta_recon_row = jnp.maximum(nominal_row_pitch, det_pixel_pitch_iso) # Ensure that the row resolution is not higher than the (x,z) detector resolution
         delta_recon_row = float(delta_recon_row)
 
         # Compute cube = (width, depth, height) of the scanned region in ALU