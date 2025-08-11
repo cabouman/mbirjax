e66e930d0ce77871e6154e2babd4df5235b5cb52
caden-cardell <65239671+caden-cardell@users.noreply.github.com>
2025-08-11 19:49:09 -0400

    clean up shepp logan prep script

--- experiments/sharding/shepp_logan_prep.py
+++ experiments/sharding/shepp_logan_prep.py
@@ -2,14 +2,16 @@ import numpy as np
 import pickle
 import mbirjax as mj
 
-"""
-**Set the geometry parameters**
-    This code is to genreate the phantom and sinogram for the 2000, 2000, 2000 problem size
-    with the original sparse projection implementations on the CPU. There are some memory 
-    issues with sharding and generating the test phantom and sinogram.
-"""
+######################### SETUP ############################
 
-output_directory = "/scratch/gautschi/ncardel"
+# 1. Set username
+USER = "ncardel" # change the user to your username
+
+# 2. Comment out or close the viewers
+
+############################################################
+
+output_directory = f"/scratch/gautschi/{USER}"
 
 # Choose the geometry type
 model_type = 'parallel'  # 'cone' or 'parallel'
@@ -20,15 +22,13 @@ num_views = 2000
 num_det_rows = 2000
 num_det_channels = 2000
 
+
 # Generate simulated data
 phantom, sinogram, params = mj.generate_demo_data(object_type=object_type, model_type=model_type,
                                                   num_views=num_views, num_det_rows=num_det_rows,
                                                   num_det_channels=num_det_channels)
 
-mj.slice_viewer(phantom, title="Phantom Test")
-
-mj.slice_viewer(sinogram, title="Sinogram Test")
-
+# save the phantom, sinogram, and params
 phantom = np.array(phantom)
 np.save(f"{output_directory}/phantom.npy", phantom)
 
@@ -37,3 +37,8 @@ np.save(f"{output_directory}/sinogram.npy", sinogram)
 
 with open(f"{output_directory}/params.pkl", "wb") as f:
     pickle.dump(params, f)
+
+# view the phantom and sinogram
+mj.slice_viewer(phantom, title="Phantom Test")
+
+mj.slice_viewer(sinogram, title="Sinogram Test")
