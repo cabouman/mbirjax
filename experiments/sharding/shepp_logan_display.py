d8caca57cbf66f81c72d4c88426bbed75f120c67
Caden Cardell <cadencardell@gmail.com>
2025-08-13 12:28:05 -0400

    adds display script

---
+++ experiments/sharding/shepp_logan_display.py
@@ -0,0 +1,43 @@
+import numpy as np
+import time
+import pickle
+import mbirjax as mj
+
+# SETUP
+# 1. Generate the pho\antom, sinogram, and params
+#    On the 'sharing_dev/stable' branch run the script 'expirements/sharding/shepp_logan_prep.py'
+#    This generates the phantom and sinogram using the CPU and saves them to the scratch directory.
+
+# 2. Set username
+USER = "ncardel" # change the user to your username
+
+# 3. Comment out or close the viewer
+
+
+###### The
+
+# load phantom, sinogram, and params
+output_directory = f"/scratch/gautschi/{USER}"
+
+phantom_filepath = f"{output_directory}/phantom.npy"
+params_filepath = f"{output_directory}/params.pkl"
+
+print("phantom_filepath:", phantom_filepath)
+print("params_filepath:", params_filepath)
+
+phantom = np.load(phantom_filepath)
+with open(params_filepath, "rb") as f:
+    params = pickle.load(f)
+
+print("phantom.shape:", phantom.shape)
+print("params:", params.items())
+
+filepath = f"{output_directory}/demo1_recon_100.h5"
+
+# The recon and recon_dict can be reloaded either here or in the viewer, and the recon_dict can be used to recreate
+#  the model if desired. The load function can be used even without an existing instance of a ct model.
+new_recon, new_recon_dict, _ = mj.TomographyModel.load_recon_hdf5(filepath, recreate_model=True)
+
+# Display results
+title = 'Phantom (left) vs Recon using sharding (right)'
+mj.slice_viewer(phantom, new_recon, data_dicts=[None, new_recon_dict], title=title)
