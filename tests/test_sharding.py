828bf855e245d80d2cc0acdb0f40790861e7fe27
Caden Cardell <cadencardell@gmail.com>
2025-08-14 04:11:15 -0400

    create first pass on testing

---
+++ tests/test_sharding.py
@@ -0,0 +1,24 @@
+import warnings
+import numpy as np
+import jax.numpy as jnp
+import mbirjax as mj
+import unittest
+
+
+class TestSharding(unittest.TestCase):
+    """
+    Test various operations done with sharding
+    """
+
+    def sparseForwardProjection(self):
+        """Set up before each test method."""
+        pass
+
+    def sparseBackProjection(self):
+        """Clean up after each test method."""
+        pass
+
+
+
+if __name__ == '__main__':
+    unittest.main()
