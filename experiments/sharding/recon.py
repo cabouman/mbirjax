2369011d4da3b1e61ad3d18dc5dc580156034cc4
Caden Cardell <cadencardell@gmail.com>
2025-10-22 20:42:35 -0400

    wip: recon

--- experiments/sharding/recon.py
+++ experiments/sharding/recon.py
@@ -1,8 +1,9 @@
 import sys
 import os
 import time
-import numpy as np
 import csv
+import h5py
+import jax
 
 # ignore the TkAgg warning to clean up the output
 import warnings
@@ -11,7 +12,7 @@ warnings.filterwarnings(
     message="TkAgg not available. Falling back to Agg."
 )
 
-def forward_project(size, num_gpus, model_type, output_filepath='output.csv'):
+def recon(size, num_gpus, model_type, output_filepath='output.csv'):
 
     # set the visible cuda devices to 0 to num_gpus-1
     os.environ["CUDA_VISIBLE_DEVICES"] = ",".join([str(x) for x in range(num_gpus)])
@@ -22,63 +23,43 @@ def forward_project(size, num_gpus, model_type, output_filepath='output.csv'):
 
     os.environ['XLA_PYTHON_CLIENT_MEM_FRACTION'] = '0.90'
 
-    # sinogram shape
-    num_views, num_det_rows, num_det_channels = size, size, size
-    sinogram_shape = (num_views, num_det_rows, num_det_channels)
+    output_directory = f"/scratch/gautschi/ncardel/recon_time"
+    h5_path = f"{output_directory}/{model_type}_{size}_projection_data.h5"
+    with h5py.File(h5_path, "r") as f:
+        phantom = f["phantom"][:]
+        sinogram = f["sinogram"][:]
+        params = f.attrs["params"]
 
-    # angles
-    start_angle = -np.pi * (1/2)
-    end_angle = np.pi * (1/2)
-    angles = jnp.linspace(start_angle, end_angle, num_views, endpoint=False)
-
-    # recon model
-    forward_projection_model = mj.ParallelBeamModel(sinogram_shape, angles)
-    forward_projection_model.set_params(sharpness=0.0, use_gpu='automatic')
-
-    # recon model
     if model_type == 'cone':
-        source_detector_dist = 4 * num_det_channels
-        source_iso_dist = source_detector_dist
-        forward_projection_model = mj.ConeBeamModel(sinogram_shape, angles, source_detector_dist=source_detector_dist,
-                                    source_iso_dist=source_iso_dist)
+        recon_model = mj.ConeBeamModel.from_file(params)
     else:
-        forward_projection_model = mj.ParallelBeamModel(sinogram_shape, angles)
-
-    forward_projection_model.set_params(sharpness=0.0, use_gpu='automatic')
-
-    # Print out model parameters
-    print("MODEL PARAMS:")
-    forward_projection_model.print_params()
-
-    # get partition pixel indices
-    recon_shape, granularity = forward_projection_model.get_params(['recon_shape', 'granularity'])
-    partitions = mj.gen_set_of_pixel_partitions(recon_shape, granularity)
-    pixel_indices = partitions[0][0]
-
-    # Generate simulated data
-    recon = jnp.ones_like(2, shape=recon_shape, device=forward_projection_model.main_device)
+        recon_model = mj.ParallelBeamModel.from_file(params)
 
     print("\nTEST PARAMS:")
-    transfer_pixel_batch_size = forward_projection_model.transfer_pixel_batch_size
-    pixel_indices_size = pixel_indices.size
-    print("Device set:", forward_projection_model.sinogram_device.device_set)
+    transfer_pixel_batch_size = recon_model.transfer_pixel_batch_size
+    print("Device set:", recon_model.sinogram_device.device_set)
     print("Transfer pixel batch size:", transfer_pixel_batch_size)
-    print("Pixel indices size:", pixel_indices_size)
 
     print("\nGPU STARTING MEMORY STATS:")
     mj.get_memory_stats()
 
-    print("\nSTARTING FORWARD PROJECTION FIRST PASS:")
-    voxel_values = forward_projection_model.get_voxels_at_indices(recon, pixel_indices)
-    forward_projection = forward_projection_model.sparse_forward_project(voxel_values, pixel_indices, output_device=forward_projection_model.sinogram_device)
-    forward_projection.block_until_ready()
+    print("\nSTARTING RECON FIRST PASS:")
+    recon_model.set_params(use_gpu="automatic")
+    recon, _ = recon_model.recon(sinogram,
+                                 max_iterations=8,
+                                 stop_threshold_change_pct=0)
+    recon.block_until_ready()
+    recon = jax.device_put(recon)
 
     # time the second pass to remove time for jitting the functions
-    print("\nSTARTING FORWARD PROJECTION SECOND PASS:")
+    print("\nSTARTING RECON SECOND PASS:")
     time0 = time.time()
-    forward_projection = forward_projection_model.sparse_forward_project(voxel_values, pixel_indices, output_device=forward_projection_model.sinogram_device)
-    forward_projection.block_until_ready()
+    recon, _ = recon_model.recon(sinogram,
+                                 max_iterations=8,
+                                 stop_threshold_change_pct=0)
+    recon.block_until_ready()
     elapsed = time.time() - time0
+    recon = jax.device_put(recon)
 
     print("\nGPU FINAL MEMORY STATS:")
     mem_stats = mj.get_memory_stats()
@@ -87,10 +68,10 @@ def forward_project(size, num_gpus, model_type, output_filepath='output.csv'):
     if not os.path.exists(output_filepath):
         with open(output_filepath, "w", newline="") as f:
             writer = csv.writer(f)
-            writer.writerow(['size', 'model_type', 'transfer_pixel_batch_size', 'pixel_indices_size', 'num_gpus', 'elapsed', 'gpu0_peak_bytes', 'gpu1_peak_bytes', 'gpu2_peak_bytes', 'gpu3_peak_bytes', 'gpu4_peak_bytes', 'gpu5_peak_bytes', 'gpu6_peak_bytes', 'gpu7_peak_bytes'])
+            writer.writerow(['size', 'model_type', 'transfer_pixel_batch_size', 'num_gpus', 'elapsed', 'gpu0_peak_bytes', 'gpu1_peak_bytes', 'gpu2_peak_bytes', 'gpu3_peak_bytes', 'gpu4_peak_bytes', 'gpu5_peak_bytes', 'gpu6_peak_bytes', 'gpu7_peak_bytes'])
 
     # append this test data to the output file
-    row = [ size, model_type, transfer_pixel_batch_size, pixel_indices_size, num_gpus, elapsed ] + [ mem_stats[i]['peak_bytes_in_use'] for i in range(num_gpus) ]
+    row = [ size, model_type, transfer_pixel_batch_size, num_gpus, elapsed ] + [ mem_stats[i]['peak_bytes_in_use'] for i in range(num_gpus) ]
     with open(output_filepath, "a", newline="") as f:
         writer = csv.writer(f)
         writer.writerow(row)
@@ -108,7 +89,7 @@ if __name__ == "__main__":
         size = 256
         num_gpus = 2
         model_type = 'cone'
-        output_filepath = "../output/forward_project.csv"
+        output_filepath = "../output/recon.csv"
 
     # the sinogram and recon will have shape (size, size, size)
-    forward_project(size, num_gpus, model_type, output_filepath=output_filepath)
+    recon(size, num_gpus, model_type, output_filepath=output_filepath)
