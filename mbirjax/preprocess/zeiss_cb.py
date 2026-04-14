Fix bug in sinogram computation step

mbirjax/preprocess/zeiss_cb.py
@@ -86,7 +86,10 @@ def compute_sino_and_params(dataset_dir, downsample_factor=(1, 1), subsample_vie
     if verbose > 0:
         print("\n\n########## Computing sinogram from object, blank, and dark scans")
 
-    sino = mjp.compute_sino_transmission(obj_scan, blank_scan, dark_scan, defective_pixel_array)
+    if np.allclose(blank_scan, 1.0):
+        sino = obj_scan
+    else:
+        sino = mjp.compute_sino_transmission(obj_scan, blank_scan, dark_scan, defective_pixel_array)
 
     if verbose > 0:
         print("\n\n########## Correcting sinogram data to account for background offset and sino offset")