faf596690084f50d879b77a0a0be4448c12c37bd
Chowdhury <samin_nur@analysis-node22.sns.gov>
2026-04-14 06:41:12 -0400

    Included demo for fast hyper recon. Updated simulation to enable hyperspectral CT data. Improved initialization within dehydrate.

--- experiments/hsnt/example_1_denoise_simulated_data.py
+++ experiments/hsnt/example_1_denoise_simulated_data.py
@@ -21,8 +21,8 @@ def main():
     # Simulation parameters
     detector_rows = 64  # Number of rows in the detector
     detector_columns = 64  # Number of columns in the detector
-    dosage_rate = 100  # Neutron dosage rate
-    material_thickness = {"Ni": 2.0, "Cu": 2.0, "Al": 10.0}  # Define material thicknesses (cm)
+    dosage_rate = 250  # Neutron dosage rate
+    material_density = {"Ni": 0.25, "Cu": 0.25, "Al": 0.75}  # Define material density (vol. fraction)
     dataset_type = 'attenuation'  # Choose between 'attenuation' or 'transmission'
 
     # Denoiser parameters
@@ -34,8 +34,8 @@ def main():
     display_pix_idx = [10, 32]  # Pixel index [row, column] of displayed spectra
     vmax = 2.5  # Maximum pixel value for displayed images
     vmin = 0  # Minimum pixel value for displayed images
-    y_lim_attenuation = (0.75, 2.5)  # (y_min, y_max) to set y-axis range for attenuation spectra
-    y_lim_transmission = (0, 0.5)  # (y_min, y_max) to set y-axis range for transmission spectra
+    y_lim_attenuation = (1, 2.7)  # (y_min, y_max) to set y-axis range for attenuation spectra
+    y_lim_transmission = (0, 0.35)  # (y_min, y_max) to set y-axis range for transmission spectra
 
     # Fix seed for random number generation
     np.random.seed(129)
@@ -46,12 +46,12 @@ def main():
     material_basis = np.load(filename)
 
     # Generate simulated noisy hyperspectral data and ground truth
-    [noisy_hyper_projection, gt_hyper_projection] = generate_hyper_data(material_basis,
-                                                                        detector_rows=detector_rows,
-                                                                        detector_columns=detector_columns,
-                                                                        dosage_rate=dosage_rate,
-                                                                        material_thickness=material_thickness,
-                                                                        verbose=verbose)
+    [noisy_hyper_projection, _, gt_hyper_projection] = generate_hyper_data(material_basis,
+                                                                           detector_rows=detector_rows,
+                                                                           detector_columns=detector_columns,
+                                                                           dosage_rate=dosage_rate,
+                                                                           material_density=material_density,
+                                                                           verbose=verbose)
 
     # Perform hyperspectral denoising (dehydrate + rehydrate)
     denoised_hyper_projection = hyper_denoise(noisy_hyper_projection,
@@ -61,24 +61,24 @@ def main():
 
     # Plot hyperspectral projections and spectra
     if verbose > 1:
-        plot_images(images=[gt_hyper_projection[:, :, display_wave_idx],
-                            noisy_hyper_projection[:, :, display_wave_idx],
-                            denoised_hyper_projection[:, :, display_wave_idx]],
+        plot_images(images=[gt_hyper_projection[0, :, :, display_wave_idx],
+                            noisy_hyper_projection[0, :, :, display_wave_idx],
+                            denoised_hyper_projection[0, :, :, display_wave_idx]],
                     titles=['Fig (a): Ground truth hyperspectral projection \n\nWavelength index: ' + str(display_wave_idx),
                             'Fig (b): Noisy hyperspectral projection \n\nWavelength index: ' + str(display_wave_idx),
                             'Fig (c): Denoised hyperspectral projection \n\nWavelength index: ' + str(display_wave_idx)],
                     vmax=vmax, vmin=vmin)
 
-        plot_spectra(spectra=[noisy_hyper_projection[display_pix_idx[0], display_pix_idx[1], :],
-                              denoised_hyper_projection[display_pix_idx[0], display_pix_idx[1], :]],
+        plot_spectra(spectra=[noisy_hyper_projection[0, display_pix_idx[0], display_pix_idx[1], :],
+                              denoised_hyper_projection[0, display_pix_idx[0], display_pix_idx[1], :]],
                      labels=['Noisy', 'Denoised'],
                      title='Single pixel spectra (attenuation) for noisy and denoised data',
                      x_label='wavelength index',
                      y_label='attenuation',
                      y_lim=y_lim_attenuation)
 
-        plot_spectra(spectra=[np.exp(-noisy_hyper_projection[display_pix_idx[0], display_pix_idx[1], :]),
-                              np.exp(-denoised_hyper_projection[display_pix_idx[0], display_pix_idx[1], :])],
+        plot_spectra(spectra=[np.exp(-noisy_hyper_projection[0, display_pix_idx[0], display_pix_idx[1], :]),
+                              np.exp(-denoised_hyper_projection[0, display_pix_idx[0], display_pix_idx[1], :])],
                      labels=['Noisy', 'Denoised'],
                      title='Single pixel spectra (transmission) for noisy and denoised data',
                      x_label='wavelength index',
