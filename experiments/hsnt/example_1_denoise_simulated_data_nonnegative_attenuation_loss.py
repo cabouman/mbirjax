a63103637a156e3163fe50e281722a3492b25f9f
Harel Dor <hareldor@gmail.com>
2026-05-11 13:46:57 -0400

    Merge in changes to example 1, add multiplicative update

--- experiments/hsnt/example_1_denoise_simulated_data_nonnegative_attenuation_loss.py
+++ experiments/hsnt/example_1_denoise_simulated_data_nonnegative_attenuation_loss.py
@@ -19,10 +19,11 @@ def main():
     start_time = time.time()
 
     # Simulation parameters
+    num_angles = 1  # Number of projection angles
     detector_rows = 64  # Number of rows in the detector
     detector_columns = 64  # Number of columns in the detector
-    dosage_rate = 100  # Neutron dosage rate
-    material_thickness = {"Ni": 2.0, "Cu": 2.0, "Al": 10.0}  # Define material thicknesses (cm)
+    dosage_rate = 250  # Neutron dosage rate
+    material_density = {"Ni": 0.25, "Cu": 0.25, "Al": 0.75}  # Define material density (vol. fraction)
     dataset_type = 'attenuation'  # Choose between 'attenuation' or 'transmission'
 
     # Denoiser parameters
@@ -34,8 +35,8 @@ def main():
     display_pix_idx = [10, 32]  # Pixel index [row, column] of displayed spectra
     vmax = 2.5  # Maximum pixel value for displayed images
     vmin = 0  # Minimum pixel value for displayed images
-    y_lim_attenuation = (0.75, 2.5)  # (y_min, y_max) to set y-axis range for attenuation spectra
-    y_lim_transmission = (0, 0.5)  # (y_min, y_max) to set y-axis range for transmission spectra
+    y_lim_attenuation = (1, 2.7)  # (y_min, y_max) to set y-axis range for attenuation spectra
+    y_lim_transmission = (0, 0.35)  # (y_min, y_max) to set y-axis range for transmission spectra
 
     # Fix seed for random number generation
     np.random.seed(129)
@@ -46,57 +47,71 @@ def main():
     material_basis = np.load(filename)
 
     # Generate simulated noisy hyperspectral data and ground truth
-    [noisy_hyper_projection, gt_hyper_projection] = generate_hyper_data(material_basis,
-                                                                        detector_rows=detector_rows,
-                                                                        detector_columns=detector_columns,
-                                                                        dosage_rate=dosage_rate,
-                                                                        material_thickness=material_thickness,
-                                                                        verbose=verbose)
+    [noisy_hyper_projection, _, gt_hyper_projection] = generate_hyper_data(material_basis,
+                                                                           num_angles=num_angles,
+                                                                           detector_rows=detector_rows,
+                                                                           detector_columns=detector_columns,
+                                                                           dosage_rate=dosage_rate,
+                                                                           material_density=material_density,
+                                                                           verbose=verbose)
 
     # Perform hyperspectral denoising (dehydrate + rehydrate)
-    W = np.random.rand(detector_rows * detector_columns, num_materials)  # Random initialization of W
-    H = np.random.rand(num_materials, gt_hyper_projection.shape[2])  # Random initialization of H
-    T = np.exp(-noisy_hyper_projection).reshape(-1, gt_hyper_projection.shape[2])
+    W = np.random.rand(num_angles * detector_rows * detector_columns, num_materials)  # Random initialization of W
+    H = np.random.rand(num_materials, gt_hyper_projection.shape[-1])  # Random initialization of H
+    T = np.exp(-noisy_hyper_projection).reshape(-1, gt_hyper_projection.shape[-1])
     learning_rate = 0.2
+    solver = 'Newton'  # Choose between 'MU' (multiplicative updates) or 'Newton' (quasi-newton descent)
     for i in range(100):  # Run for a fixed number of iterations
         Z = np.exp(-W @ H)
 
-        dL_dA = (T - Z) @ H.T
-        dL_dB = W.T @ (T - Z)
+        if solver == 'Newton':
+            dL_dA = (T - Z) @ H.T
+            dL_dB = W.T @ (T - Z)
 
-        d2L_dA2 = Z @ H.T**2
-        d2L_dB2 = W.T**2 @ Z
+            d2L_dA2 = Z @ H.T**2
+            d2L_dB2 = W.T**2 @ Z
 
-        W = np.maximum(W - learning_rate * dL_dA / (d2L_dA2 + 1e-10), 1e-10)
-        H = np.maximum(H - learning_rate * dL_dB / (d2L_dB2 + 1e-10), 1e-10)
+            W = np.maximum(W - learning_rate * dL_dA / (d2L_dA2 + 1e-10), 1e-10)
+            H = np.maximum(H - learning_rate * dL_dB / (d2L_dB2 + 1e-10), 1e-10)
+        elif solver == 'MU':
+            W_mult = ((Z @ H.T) / (T @ H.T + 1e-10) + 1) / 2
+            H_mult = ((W.T @ Z) / (W.T @ T + 1e-10) + 1) / 2
 
-    denoised_hyper_projection = (W @ H).reshape(detector_rows, detector_columns, gt_hyper_projection.shape[2])
+            W = np.maximum(W * W_mult, 1e-10)
+            H = np.maximum(H * H_mult, 1e-10)
+        else:
+            raise ValueError('Invalid solver type. Choose between "MU" and "Newton".')
+
+    denoised_hyper_projection = (W @ H).reshape(num_angles, detector_rows, detector_columns, gt_hyper_projection.shape[-1])
 
     # Plot hyperspectral projections and spectra
     if verbose > 1:
-        plot_images(images=[gt_hyper_projection[:, :, display_wave_idx],
-                            noisy_hyper_projection[:, :, display_wave_idx],
-                            denoised_hyper_projection[:, :, display_wave_idx]],
+        plot_images(images=[gt_hyper_projection[0, :, :, display_wave_idx],
+                            noisy_hyper_projection[0, :, :, display_wave_idx],
+                            denoised_hyper_projection[0, :, :, display_wave_idx]],
                     titles=['Fig (a): Ground truth hyperspectral projection \n\nWavelength index: ' + str(display_wave_idx),
                             'Fig (b): Noisy hyperspectral projection \n\nWavelength index: ' + str(display_wave_idx),
                             'Fig (c): Denoised hyperspectral projection \n\nWavelength index: ' + str(display_wave_idx)],
-                    vmax=vmax, vmin=vmin)
+                    vmax=vmax, vmin=vmin,
+                    filename=f'example_1_nonnegative_attenuation_loss_{solver}_projection.png')
 
-        plot_spectra(spectra=[noisy_hyper_projection[display_pix_idx[0], display_pix_idx[1], :],
-                              denoised_hyper_projection[display_pix_idx[0], display_pix_idx[1], :]],
-                     labels=['Noisy', 'Denoised'],
+        plot_spectra(spectra=[noisy_hyper_projection[0, display_pix_idx[0], display_pix_idx[1], :],
+                              denoised_hyper_projection[0, display_pix_idx[0], display_pix_idx[1], :]],
+                     labels=['Noisy', f'Denoised {solver}'],
                      title='Single pixel spectra (attenuation) for noisy and denoised data',
                      x_label='wavelength index',
                      y_label='attenuation',
-                     y_lim=y_lim_attenuation)
+                     y_lim=y_lim_attenuation,
+                     filename=f'example_1_nonnegative_attenuation_loss_{solver}_spectra.png')
 
-        plot_spectra(spectra=[np.exp(-noisy_hyper_projection[display_pix_idx[0], display_pix_idx[1], :]),
-                              np.exp(-denoised_hyper_projection[display_pix_idx[0], display_pix_idx[1], :])],
-                     labels=['Noisy', 'Denoised'],
+        plot_spectra(spectra=[np.exp(-noisy_hyper_projection[0, display_pix_idx[0], display_pix_idx[1], :]),
+                              np.exp(-denoised_hyper_projection[0, display_pix_idx[0], display_pix_idx[1], :])],
+                     labels=['Noisy', f'Denoised {solver}'],
                      title='Single pixel spectra (transmission) for noisy and denoised data',
                      x_label='wavelength index',
                      y_label='transmission',
-                     y_lim=y_lim_transmission)
+                     y_lim=y_lim_transmission,
+                     filename=f'example_1_nonnegative_attenuation_loss_{solver}_spectra_transmission.png')
 
     print('Total time elapsed: ', time.time() - start_time, ' seconds')
 
