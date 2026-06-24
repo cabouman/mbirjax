708dd18a5ccef1d68d5ab56d6010a93ef6974ad0
Greg Buzzard <buzzard@purdue.edu>
2026-06-23 22:25:58 -0400

    Make _get_sino_indicator consistent in denoiser.

--- mbirjax/tomography_model.py
+++ mbirjax/tomography_model.py
@@ -2532,6 +2532,7 @@ class TomographyModel(ParameterHandler):
 
         Args:
             sinogram (ndarray): 3D jax array containing sinogram with shape (num_views, num_det_rows, num_det_channels).
+            verbose (int, optional): Verbosity level. Defaults to 1.
 
         Returns:
             (ndarray): Weights used in mbircone reconstruction, with the same array shape as ``sinogram``.
